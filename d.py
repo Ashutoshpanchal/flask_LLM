6bddd81b66458946939f3a87bef069f43b477eee
Ashutosh Panchal <ashutoshpanchal@192.168.1.7>
2025-01-28 00:24:49 +0530

    change

--- d.py
+++ d.py
@@ -1,7 +1,7 @@
 from huggingface_hub import hf_hub_download
 import os
 import os
-os.environ['HF_TOKEN'] = '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'
+# os.environ['HF_TOKEN'] = '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'
 def download_model():
     repo_id = "chiizaraa/Llama-3.2-1B-Instruct-Q4_K_M-GGUF"
     filename = "llama-3.2-1b-instruct-q4_k_m.gguf"
